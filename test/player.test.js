908ac443fc9ca74b10cadfdfacd20feeeb8d27ea
Predrag Stojadinovic <predrags@nvidia.com>
2018-05-30 12:25:27 +0200

    finishing player testing

--- test/player.test.js
+++ test/player.test.js
@@ -1,10 +1,51 @@
 const _ = require('lodash');
 const expect = require('chai').expect;
 
-let Player = require('../lib/rating');
+let Player = require('../lib/player');
+let p1 = new Player(50).setRatingChange(100);
+let p2 = new Player(200, 1100).setRatingChange(-100);
 
 describe('Player tests', function () {
 	it('Player should exist', function () {
 		expect(Player).to.exist;
+	});
+
+	describe('Player constructor tests', function () {
+		it('contructor should create object', function () {
+			expect(() => new Player()).to.throw();
+			expect(() => new Player(50)).to.not.throw();
+			expect(new Player(50)).to.be.a('object');
+		});
+	});
+
+	describe('Player setter tests', function () {
+		it('setRatingChange should work properly', function () {
+			expect(() => new Player(0).setRatingChange()).to.throw();
+			expect(() => new Player(0).setRatingChange(100)).to.not.throw();
+			expect(new Player(0).setRatingChange(-100)).to.be.a('object');
+		});
+	});
+
+	describe('Player getters tests', function () {
+		it('getScore should work properly', function () {
+			expect(p1.getScore()).to.equal(50);
+			expect(p2.getScore()).to.equal(200);
+		});
+		it('getOldRating should work properly', function () {
+			expect(p1.getOldRating()).to.equal(1000);
+			expect(p2.getOldRating()).to.equal(1100);
+		});
+		it('getNewRating should work properly', function () {
+			expect(p1.getNewRating()).to.equal(1100);
+			expect(p2.getNewRating()).to.equal(1000);
+		});
+		it('getChange should work properly', function () {
+			expect(p1.getChange()).to.equal(100);
+			expect(p2.getChange()).to.equal(-100);
+		});
+		it('getData should work properly', function () {
+			expect(p1.getData()).to.deep.equal({score: 50, oldRating: 1000, newRating: 1100, change: 100});
+			expect(p2.getData()).to.deep.equal({score: 200, oldRating: 1100, newRating: 1000, change: -100});
+		});
 	});
 });
--- test/rating.test.js
+++ test/rating.test.js
@@ -1,6 +1,11 @@
 const _ = require('lodash');
 const expect = require('chai').expect;
 
+const rewire = require("rewire");
+const _Rating = rewire('../lib/rating.js');
+
+// console.log('getDs', _Rating.__get__('D'));
+
 let Rating = require('../lib/rating');
 
 describe('Rating tests', function () {
