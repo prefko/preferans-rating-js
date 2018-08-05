e70e77714ad7c3615b43a803a850a1f73a3c8a10
Predrag Stojadinovic <predrag@stojadinovic.net>
2018-08-05 20:02:05 +0200

    finalizing the player

--- test/player.test.js
+++ test/player.test.js
@@ -1,9 +1,7 @@
 const _ = require('lodash');
 const expect = require('chai').expect;
 
-let Player = require('../lib/player');
-let p1 = new Player(50).setRatingChange(100);
-let p2 = new Player(200, 1100).setRatingChange(-100);
+const Player = require('../lib/player');
 
 describe('Player tests', function () {
 	it('Player should exist', function () {
@@ -19,14 +17,26 @@ describe('Player tests', function () {
 	});
 
 	describe('Player setter tests', function () {
+		it('setNewRating should work properly', function () {
+			expect(() => new Player(0).setNewRating()).to.throw();
+			expect(() => new Player(0).setNewRating(1100)).to.not.throw();
+			expect(new Player(0).setNewRating(1100)).to.be.a('object');
+			expect(new Player(0).setNewRating(1100).getChange()).to.equal(100);
+			expect(new Player(0).setNewRating(1100).getNewRating()).to.equal(1100);
+		});
 		it('setRatingChange should work properly', function () {
 			expect(() => new Player(0).setRatingChange()).to.throw();
 			expect(() => new Player(0).setRatingChange(100)).to.not.throw();
-			expect(new Player(0).setRatingChange(-100)).to.be.a('object');
+			expect(new Player(0).setRatingChange(100)).to.be.a('object');
+			expect(new Player(0).setRatingChange(100).getChange()).to.equal(100);
+			expect(new Player(0).setRatingChange(100).getNewRating()).to.equal(1100);
 		});
 	});
 
 	describe('Player getters tests', function () {
+		let p1 = new Player(50).setRatingChange(100);
+		let p2 = new Player(200, 1100).setRatingChange(-100);
+
 		it('getScore should work properly', function () {
 			expect(p1.getScore()).to.equal(50);
 			expect(p2.getScore()).to.equal(200);
