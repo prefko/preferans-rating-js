506744cdc3cc85de5d1baa97a0fe649a18eae55d
Predrag Stojadinovic <predrags@nvidia.com>
2019-02-06 14:35:02 +0100

    refactoring

--- src/prefRating.ts
+++ src/prefRating.ts
@@ -4,10 +4,8 @@
 import * as math from 'mathjs';
 import PrefRatingPlayer, {PrefRatingPlayerObject} from './prefRatingPlayer';
 
-export type PrefRatingDs = { D12: number, D13: number, D23: number }
-export type PrefRatingTs = { T12: number, T13: number, T23: number }
-export type PrefRatingNs = { N12: number, N13: number, N23: number }
-export type PrefRatingChanges = { change1: number, change2: number, change3: number }
+export type PrefRatingCalc = { a12: number, a13: number, a23: number }
+export type PrefRatingChanges = { c1: number, c2: number, c3: number }
 export type PrefRatingObject = { bula: number, p1: PrefRatingPlayerObject, p2: PrefRatingPlayerObject, p3: PrefRatingPlayerObject }
 
 /**
@@ -26,10 +24,10 @@ const calcD = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, bula: number): number
 			.done(),
 		3,
 	));
-const calculateDs = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer, bula: number): PrefRatingDs => ({
-	D12: calcD(p1, p2, bula),
-	D13: calcD(p1, p3, bula),
-	D23: calcD(p2, p3, bula),
+const calculateDs = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer, bula: number): PrefRatingCalc => ({
+	a12: calcD(p1, p2, bula),
+	a13: calcD(p1, p3, bula),
+	a23: calcD(p2, p3, bula),
 });
 
 const calcT = (p1: PrefRatingPlayer, p2: PrefRatingPlayer): number =>
@@ -42,17 +40,17 @@ const calcT = (p1: PrefRatingPlayer, p2: PrefRatingPlayer): number =>
 			.done(),
 		3,
 	));
-const calculateTs = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer): PrefRatingTs => ({
-	T12: calcT(p1, p2),
-	T13: calcT(p1, p3),
-	T23: calcT(p2, p3),
+const calculateTs = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer): PrefRatingCalc => ({
+	a12: calcT(p1, p2),
+	a13: calcT(p1, p3),
+	a23: calcT(p2, p3),
 });
 
 const calcN = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, bula: number): number => (p1.score > p2.score ? bula : p1.score < p2.score ? -bula : 0) / 100;
-const calculateNs = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer, bula: number): PrefRatingNs => ({
-	N12: calcN(p1, p2, bula),
-	N13: calcN(p1, p3, bula),
-	N23: calcN(p2, p3, bula),
+const calculateNs = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer, bula: number): PrefRatingCalc => ({
+	a12: calcN(p1, p2, bula),
+	a13: calcN(p1, p3, bula),
+	a23: calcN(p2, p3, bula),
 });
 
 const calcC = (c1: number, c2: number): number =>
@@ -64,18 +62,18 @@ const calcC = (c1: number, c2: number): number =>
 			.done(),
 	));
 const calculateChanges = (p1: PrefRatingPlayer, p2: PrefRatingPlayer, p3: PrefRatingPlayer, bula: number): PrefRatingChanges => {
-	const D: PrefRatingDs = calculateDs(p1, p2, p3, bula);
-	const T: PrefRatingTs = calculateTs(p1, p2, p3);
-	const N: PrefRatingNs = calculateNs(p1, p2, p3, bula);
+	const D: PrefRatingCalc = calculateDs(p1, p2, p3, bula);
+	const T: PrefRatingCalc = calculateTs(p1, p2, p3);
+	const N: PrefRatingCalc = calculateNs(p1, p2, p3, bula);
 
-	const C12 = D.D12 + T.T12 + N.N12;
-	const C13 = D.D13 + T.T13 + N.N13;
-	const C23 = D.D23 + T.T23 + N.N23;
+	const C12 = D.a12 + T.a12 + N.a12;
+	const C13 = D.a13 + T.a13 + N.a13;
+	const C23 = D.a23 + T.a23 + N.a23;
 
 	return {
-		change1: calcC(C12, C13),
-		change2: calcC(-C12, C23),
-		change3: calcC(-C13, -C23),
+		c1: calcC(C12, C13),
+		c2: calcC(-C12, C23),
+		c3: calcC(-C13, -C23),
 	};
 };
 
@@ -105,10 +103,10 @@ export default class PrefRating {
 		this._p3 = p3;
 		this._bula = bula;
 
-		const {change1, change2, change3} = calculateChanges(p1, p2, p3, bula);
-		this._p1.change = change1;
-		this._p2.change = change2;
-		this._p3.change = change3;
+		const {c1, c2, c3} = calculateChanges(p1, p2, p3, bula);
+		this._p1.change = c1;
+		this._p2.change = c2;
+		this._p3.change = c3;
 	}
 
 	/** Getter.
