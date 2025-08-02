0282f8dab8b01acc1888f71fe15cac11be02d7cd
Predrag Stojadinovic <predrags@nvidia.com>
2025-08-02 17:14:58 +0200

    chore: cleanup

--- test/prefRating.types.spec.ts
+++ test/prefRating.types.spec.ts
@@ -1,9 +1,6 @@
 'use strict';
 
-import {expect} from 'chai';
-
 describe('prefRating.types tests', () => {
-	expect(true).to.be.true;
 	// TODO: implement tests
 	it('should be implemented');
 });
--- test/prefRatingPlayer.spec.ts
+++ test/prefRatingPlayer.spec.ts
@@ -1,9 +1,6 @@
 'use strict';
 
-import {expect} from 'chai';
-
 describe('prefRatingPlayer tests', () => {
-	expect(true).to.be.true;
 	// TODO: implement tests
 	it('should be implemented');
 });
